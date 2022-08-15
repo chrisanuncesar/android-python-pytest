fb66f20559f7d4318f91437f7b4210acb1af1df8
bbchriscesar <43531282+bbchriscesar@users.noreply.github.com>
2022-08-15 21:39:39 +0800

    Deleted all files, will upload a complete compy

--- Tollway/config/Config.py
+++
@@ -1,5 +0,0 @@
-class TestData:
-    BASE_URL = "https://www.google.com/"
-    USERNAME = "admin"
-    PASSWORD = "admin"
-    HOMEPAGE_TITLE = "Google"
--- Tollway/config/Utils.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Utilities:
-
-    @staticmethod
-    def get_current_dir():
-        return os.getcwd()
